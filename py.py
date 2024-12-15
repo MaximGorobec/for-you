ef6f5b57c2c4e8a58de068070c910cac498d90b0
Ordo_max <gorobec8800@gmail.com>
2024-12-15 08:49:41 +0300

    к клтку

--- py.py
+++ py.py
@@ -1,60 +1,37 @@
-import pygame
 from pygame import Color
-class Board:
-    # создание поля
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * height for _ in range(width)]
-        # значения по умолчанию
-        self.left = 20
-        self.top = 50
-        self.cell_size = 30
+from random import randint
+import pygame
 
-    # настройка внешнего вида
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
-    def render(self, screen):
-        for y in range(self.height):
-            for x in range(self.width):
-                r = 99
-                if self.board[x][y] == 0:
-                    r = 1
-                    c = Color('white')
-                elif self.board[x][y] == 1:
-                    c = Color('Red')
-                else:
-                    c = Color('Blue')
-                pygame.draw.rect(screen, c, (x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size, self.cell_size), r)
-    def get_cell(self, mouse_pos):
-        pos = ((mouse_pos[0] - self.left) // self.cell_size, (mouse_pos[1] - self.top) // self.cell_size)
-        if 0 <= pos[0] <= self.width - 1 and 0 <=  pos[1] <= self.height - 1:
-            return pos
-        return None
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        if cell != None:
-            self.on_click(cell)
-
-    def on_click(self, cell):
-        self.board[cell[0]][cell[1]] = (self.board[cell[0]][cell[1]] + 1) % 3
+class Ball():
+    def __init__(self, pos, vec):
+        self.pos = list(pos)
+        self.vec = list(vec)
+        self.r = 10
 
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Движущийся круг 2')
-    size = width, height = 800, 400
+    size = width, height = 501, 501
     screen = pygame.display.set_mode(size)
-    board = Board(5, 5)
+    ball_pos = [width // 2, height // 2]
+    ball_n_pos = ball_pos
+    v = 10
+    fps = 60
     running = True
+    clock = pygame.time.Clock()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                board.get_click(event.pos)
-        screen.fill((0, 0, 0))
-        board.render(screen)
+            if event.type == pygame.MOUSEBUTTONUP:
+                ball_n_pos = event.pos
+        if ball_n_pos[1] != ball_pos[1]:
+            ball_pos[1] += (ball_n_pos[1] - ball_pos[1]) / abs(ball_n_pos[1] - ball_pos[1])
+        if ball_n_pos[0] != ball_pos[0]:
+            ball_pos[0] += (ball_n_pos[0] - ball_pos[0]) / abs(ball_n_pos[0] - ball_pos[0])
+
+        pygame.draw.circle(screen, Color('red'), ball_pos, 20)
         pygame.display.flip()
-    pygame.display.flip()
+        screen.fill((0, 0, 0))
+        clock.tick(fps)
+    pygame.quit()
