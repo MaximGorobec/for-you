c3a38a986f594478cf855d9f9b58b524239adeca
student <vetra2000@bk.ru>
2024-12-24 18:37:18 +0700

    red balls

--- main.py
+++ main.py
@@ -17,22 +17,36 @@ def load_image(name, colorkey=None):
         image.set_colorkey(colorkey)
     return image
 
-class Bomb(pygame.sprite.Sprite):
-    image = load_image("bomb.png")
-    image_boom = load_image("boom.png")
+class Border(pygame.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:  # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y):
+        super().__init__(all_sprites)
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"),
+                           (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
 
-    def __init__(self, group):
-        super().__init__(group)
-        self.image = Bomb.image
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(screen.get_width() - self.image.get_width())
-        self.rect.y = random.randrange(screen.get_height() - self.image.get_height())
-
-    def update(self, *args):
-        if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
-                self.rect.collidepoint(args[0].pos):
-            self.image = self.image_boom
-
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+        if pygame.sprite.spritecollideany(self, horizontal_borders):
+            self.vy = -self.vy
+        if pygame.sprite.spritecollideany(self, vertical_borders):
+            self.vx = -self.vx
 
 if __name__ == '__main__':
     pygame.init()
@@ -42,8 +56,18 @@ if __name__ == '__main__':
     running = True
 
     all_sprites = pygame.sprite.Group()
-    for _ in range(20):
-        Bomb(all_sprites)
+
+
+    horizontal_borders = pygame.sprite.Group()
+    vertical_borders = pygame.sprite.Group()
+
+    Border(5, 5, width - 5, 5)
+    Border(5, height - 5, width - 5, height - 5)
+    Border(5, 5, 5, height - 5)
+    Border(width - 5, 5, width - 5, height - 5)
+
+    for i in range(10):
+        Ball(20, 100, 100)
 
     clock = pygame.time.Clock()
     pygame.mouse.set_visible(True)
@@ -51,11 +75,9 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                for bomb in all_sprites:
-                    bomb.update(event)
+        all_sprites.update()
         all_sprites.draw(screen)
         pygame.display.flip()
-        screen.fill((0, 0, 0))
+        screen.fill((255, 255, 255))
         clock.tick(20)
     pygame.display.flip()
