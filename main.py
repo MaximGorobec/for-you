ece8789ae2ce7b13ef0aedb4212d93946265263b
student <vetra2000@bk.ru>
2025-01-14 19:03:52 +0700

    mario

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 import pygame
-import os
 import sys
-import random
-from pygame import Color
+import os
+FPS = 50
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     # если файл не существует, то выходим
@@ -17,48 +39,73 @@ def load_image(name, colorkey=None):
         image.set_colorkey(colorkey)
     return image
 
-class Bomb(pygame.sprite.Sprite):
-    image = load_image("bomb2.png")
-    image_boom = load_image("boom.png")
+player = None
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 
-    def __init__(self, group):
-        self.image = Bomb.image
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(screen.get_width() - self.image.get_width())
-        self.rect.y = random.randrange(screen.get_height() - self.image.get_height())
-        while pygame.sprite.spritecollideany(self, group):
-            self.rect.x = random.randrange(screen.get_width() - self.image.get_width())
-            self.rect.y = random.randrange(screen.get_height() - self.image.get_height())
-        super().__init__(group)
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
 
-    def update(self, *args):
-        if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
-                self.rect.collidepoint(args[0].pos):
-            self.image = self.image_boom
 
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('mario.png')
+
+tile_width = tile_height = 50
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
 
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('shaders')
-    size = width, height = 1000, 1000
+    size = width, height = pygame.image.load('data/fon.jpg').get_size()
     screen = pygame.display.set_mode(size)
     running = True
 
     all_sprites = pygame.sprite.Group()
-    for _ in range(20):
-        Bomb(all_sprites)
-
     clock = pygame.time.Clock()
     pygame.mouse.set_visible(True)
-    while running:
+    fon = pygame.transform.scale(load_image('fon.jpg'), size)
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    start = 0
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                for bomb in all_sprites:
-                    bomb.update(event)
-        all_sprites.draw(screen)
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                start = 1
+
+        if start:
+            player, level_x, level_y = generate_level(load_level('map.txt'))
+        else:
+            screen.blit(pygame.image.load('data/fon.jpg').convert(), (0, 0))
         pygame.display.flip()
-        screen.fill((0, 0, 0))
-        clock.tick(20)
-    pygame.display.flip()
+        clock.tick(FPS)
